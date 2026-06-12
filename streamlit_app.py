9aaf8b61fb1c02644f84bd133ca0eb16ebbed610
lerteper <lerteper@gmail.com>
2026-06-12 09:05:45 +0000

    stocks app iteration 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
 st.title("🎈 My new app")
+val = st.slider('slide', 0, 1000)
+data = {
+    
+}
+for i in range(0, val):
+    data[i] = i
+    data[2 * val - i] = i
+st.bar_chart(data, color = [val / 1000, (1000 - val)/1000, 0])
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
