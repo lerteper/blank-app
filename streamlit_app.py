6bcae7451d1b4b43158cc0f07d636783bc70a9f5
lerteper <lerteper@gmail.com>
2026-06-12 09:13:48 +0000

    stocks app iteration 1 (realrearlreal)

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,13 @@ slider = st.select_slider("Time Frame", ["1 week", "2 weeks", "1 month", "3 mont
 
 chosen_ticker = yf.Tickers(selection)
 
-dict = {
+if (chosen_ticker == None):
+
+    'Please select a ticker.'
+
+else:
+
+    dict = {
     "1 day" : ["1d", "1m"],
     "1 week": ["7d", "5m"], 
     "2 weeks": ["14d", "5m"],
@@ -17,13 +23,13 @@ dict = {
     "1 year": ["1y", "1d"], 
     "5 years": ["5y", "1d"],
     "10 years": ["10y", "1d"]
-}
+    }
 
-chosen_timeframe = dict[slider]
-data = chosen_ticker.history(period = chosen_timeframe[0], interval = chosen_timeframe[1])
+    chosen_timeframe = dict[slider]
+    data = chosen_ticker.history(period = chosen_timeframe[0], interval = chosen_timeframe[1])
 
-datatype = st.selectbox("Data Type", ["Open", "High", "Low", "Close", "Volume", "Dividends", "Stock Splits"])
+    datatype = st.selectbox("Data Type", ["Open", "High", "Low", "Close", "Volume", "Dividends", "Stock Splits"])
 
     
-st.line_chart(data[datatype])
+    st.line_chart(data[datatype])
 
